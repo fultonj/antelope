66d8b5160d35b6b1c5ff790431cf2da5c5d25214
John Fulton <fulton@redhat.com>
2023-07-06 14:19:32 -0400

    add clean option to rebuild.sh

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # Rebuild control plane and prepare data-plane nodes for configuration
-# Use after running clean.sh with (EDPM_{CR,NODE}, CONTROL, PVC)
 
 NODES=2
 NODE_START=0
+CLEAN=1
+
+if [ $CLEAN -eq 1 ]; then
+    # Assuming EDPM_CR EDPM_NODE CONTROL PVC are all 1
+    bash clean.sh
+fi
 
 # Deploy control plane with deps and PVCs
 export PVC=1
