9a4c3228ca6ddca3409a279ce76a8360f78e9b83
John Fulton <fulton@redhat.com>
2023-10-12 20:25:20 +0000

    --skip-tags packages,boostrap

--- cifw/deploy.sh
+++ cifw/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 pushd ~/ci-framework
-ansible-playbook deploy-edpm.yml -e @~/my-env.yml
+
+ansible-playbook deploy-edpm.yml \
+                 -e @~/my-env.yml \
+                 --skip-tags packages,boostrap
+
 popd
