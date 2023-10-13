abd32c6f03ade286c91e9eede2022733578569ba
John Fulton <fulton@redhat.com>
2023-10-13 18:15:44 +0000

    do not do crc_cleanup by default

--- cifw/clean.sh
+++ cifw/clean.sh
@@ -3,5 +3,8 @@
 pushd ~/ci-framework
 ansible-playbook cleanup-edpm.yml -e @~/my-env.yml
 rm -rf ~/ci-framework-data
-make -C $HOME/install_yamls/devsetup crc_cleanup edpm_compute_cleanup
+make -C $HOME/install_yamls/devsetup edpm_compute_cleanup # crc_cleanup
 popd
+
+# cleanup-edpm.yml basically just deletes these directories
+# https://github.com/openstack-k8s-operators/ci-framework/blob/main/ci_framework/roles/ci_setup/tasks/directories.yml
