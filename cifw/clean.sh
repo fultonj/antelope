ef7f721a11ba239323253336119f687a531459f6
John Fulton <fulton@redhat.com>
2023-10-14 22:07:35 +0000

    delete all edpm computes in cifw/clean.sh

--- cifw/clean.sh
+++ cifw/clean.sh
@@ -2,8 +2,11 @@
 
 pushd ~/ci-framework
 ansible-playbook cleanup-edpm.yml -e @~/my-env.yml
-rm -rf ~/ci-framework-data
-make -C $HOME/install_yamls/devsetup edpm_compute_cleanup # crc_cleanup
+# rm -rf ~/ci-framework-data
+for I in $(seq 0 2); do
+    make -C $HOME/install_yamls/devsetup edpm_compute_cleanup EDPM_COMPUTE_SUFFIX=$I;
+done
+# make -C $HOME/install_yamls/devsetup # crc_cleanup
 popd
 
 # cleanup-edpm.yml basically just deletes these directories
