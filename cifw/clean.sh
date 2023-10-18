0e48299bd56d686e0b880aec0ba32455f344358b
John Fulton <fulton@redhat.com>
2023-10-18 22:14:54 +0000

    run ci-framework in a venv

--- cifw/clean.sh
+++ cifw/clean.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
 pushd ~/ci-framework
+if [[ ! -d venv ]]; then
+    mkdir venv
+    python -m venv venv
+fi
+source venv/bin/activate 
+
 ansible-playbook cleanup-edpm.yml -e @~/my-env.yml
-# rm -rf ~/ci-framework-data
+rm -rf ~/ci-framework-data
+rm -rf ~/ansible_facts_cache
+mv ~/ansible.log ~/ansible.log.old
+gzip ~/ansible.log.old
+
 for I in $(seq 0 2); do
     make -C $HOME/install_yamls/devsetup edpm_compute_cleanup EDPM_COMPUTE_SUFFIX=$I;
 done
-# make -C $HOME/install_yamls/devsetup # crc_cleanup
+
+make -C $HOME/install_yamls/devsetup crc_cleanup
+
+deactivate
 popd
 
 # cleanup-edpm.yml basically just deletes these directories
--- cifw/deploy.sh
+++ cifw/deploy.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 pushd ~/ci-framework
+if [[ ! -d venv ]]; then
+    mkdir venv
+    python -m venv venv
+fi
+source venv/bin/activate 
+
+pip list
 
 ansible-playbook deploy-edpm.yml \
-                 -e @~/my-env.yml \
-                 --skip-tags packages,boostrap
+                 -e @~/my-env.yml
 
+# --skip-tags packages,boostrap,infra,test
+
+deactivate
 popd
