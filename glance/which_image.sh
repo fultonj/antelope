ce4b82e22e1d8a0cd760bf362c9be4c83a4b7e71
John Fulton <fulton@redhat.com>
2023-11-16 14:58:54 -0500

    add -v to which_image.sh to get hold old it is

--- glance/which_image.sh
+++ glance/which_image.sh
@@ -3,3 +3,15 @@
 OP=$(oc get pod -n openstack-operators | grep glance-operator-controller | awk {'print $1'})
 IMG=$(oc get pod -n openstack-operators $OP -o yaml | grep quay | tail -1 | awk {'print $2'})
 echo $IMG
+
+while getopts ":v" opt; do
+    case $opt in
+        v) verbose=true ;;
+        \?) echo "Invalid option: -$OPTARG" >&2; exit 1 ;;
+        :) echo "Option -$OPTARG requires an argument." >&2; exit 1 ;;
+    esac
+done
+if [ "$verbose" = true ]; then
+    podman pull $IMG 2> /dev/null > /dev/null
+    podman inspect -f '{{ .Created }}' $IMG
+fi
