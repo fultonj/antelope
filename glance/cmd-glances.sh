431c51b735c402cba68e42c93b9a4f0fc34436d0
John Fulton <fulton@redhat.com>
2023-11-19 11:20:28 -0500

    Add cmd-glances.sh to run commands on all glance pods
    $ ./cmd-glances.sh ls -l /var/lib/
    > glance-internal-api-0 ls -l /var/lib/
    total 0
    drwxr-xr-x. 1 root   root   20 Oct 26 06:15 alternatives
    drwxr-xr-x. 1 root   root   93 Oct 26 06:16 dnf
    drwxr-xr-x. 2 root   root    6 Aug  9  2021 games
    drwxr-xr-x. 2 glance nobody 62 Mar 31  2023 glance
    drwxr-xr-x. 4 root   root   35 Oct 26 06:16 gssproxy
    drwx------. 2 apache apache  6 Jul 20 08:43 httpd
    drwxr-xr-x. 8 root   root   90 Oct 26 06:16 iscsi
    drwxr-xr-x. 2 root   root    6 Aug  9  2021 misc
    drwxr-xr-x. 5 root   root   80 Oct 26 06:16 nfs
    drwx------. 2 root   root    6 Oct 26 06:16 private
    drwxr-xr-x. 2 rpc    rpc     6 Oct 26 06:16 rpcbind
    drwxr-xr-x. 1 root   root   74 Jun 30 14:50 rpm
    drwxr-xr-x. 2 root   root    6 Aug  9  2021 rpm-state
    drwxr-xr-x. 3 root   root   17 Oct 23 08:12 selinux
    drwxr-xr-x. 4 root   root   56 Oct 26 06:16 systemd
    drwxr-xr-x. 3 root   root   20 Oct 26 06:16 tpm2-tss
    > glance-internal-api-1 ls -l /var/lib/
    total 0
    drwxr-xr-x. 1 root   root   20 Oct 26 06:15 alternatives
    drwxr-xr-x. 1 root   root   93 Oct 26 06:16 dnf
    drwxr-xr-x. 2 root   root    6 Aug  9  2021 games
    drwxr-xr-x. 2 glance nobody 62 Mar 31  2023 glance
    drwxr-xr-x. 4 root   root   35 Oct 26 06:16 gssproxy
    drwx------. 2 apache apache  6 Jul 20 08:43 httpd
    drwxr-xr-x. 8 root   root   90 Oct 26 06:16 iscsi
    drwxr-xr-x. 2 root   root    6 Aug  9  2021 misc
    drwxr-xr-x. 5 root   root   80 Oct 26 06:16 nfs
    drwx------. 2 root   root    6 Oct 26 06:16 private
    drwxr-xr-x. 2 rpc    rpc     6 Oct 26 06:16 rpcbind
    drwxr-xr-x. 1 root   root   74 Jun 30 14:50 rpm
    drwxr-xr-x. 2 root   root    6 Aug  9  2021 rpm-state
    drwxr-xr-x. 3 root   root   17 Oct 23 08:12 selinux
    drwxr-xr-x. 4 root   root   56 Oct 26 06:16 systemd
    drwxr-xr-x. 3 root   root   20 Oct 26 06:16 tpm2-tss
    > glance-internal-api-2 ls -l /var/lib/
    total 0
    drwxr-xr-x. 1 root   root   20 Oct 26 06:15 alternatives
    drwxr-xr-x. 1 root   root   93 Oct 26 06:16 dnf
    drwxr-xr-x. 2 root   root    6 Aug  9  2021 games
    drwxr-xr-x. 2 glance nobody 62 Mar 31  2023 glance
    drwxr-xr-x. 4 root   root   35 Oct 26 06:16 gssproxy
    drwx------. 2 apache apache  6 Jul 20 08:43 httpd
    drwxr-xr-x. 8 root   root   90 Oct 26 06:16 iscsi
    drwxr-xr-x. 2 root   root    6 Aug  9  2021 misc
    drwxr-xr-x. 5 root   root   80 Oct 26 06:16 nfs
    drwx------. 2 root   root    6 Oct 26 06:16 private
    drwxr-xr-x. 2 rpc    rpc     6 Oct 26 06:16 rpcbind
    drwxr-xr-x. 1 root   root   74 Jun 30 14:50 rpm
    drwxr-xr-x. 2 root   root    6 Aug  9  2021 rpm-state
    drwxr-xr-x. 3 root   root   17 Oct 23 08:12 selinux
    drwxr-xr-x. 4 root   root   56 Oct 26 06:16 systemd
    drwxr-xr-x. 3 root   root   20 Oct 26 06:16 tpm2-tss
    $

---
+++ glance/cmd-glances.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SVC=glance-internal
+#SVC=glance-external
+#SVC=glance-single
+
+if [[ -z $@ ]]; then
+    CMD="ls -l /var/lib/"
+else
+    CMD=$@
+fi
+
+for POD in $(oc get pods -l service=$SVC --no-headers -o custom-columns=":metadata.name"); do
+    echo "> $POD $CMD"
+    oc rsh -t --shell='/bin/sh' $POD $CMD 2> /dev/null
+done
