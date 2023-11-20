8312b213eea11ed98c5880dd3b82587a0863911e
John Fulton <fulton@redhat.com>
2023-11-20 16:13:28 -0500

    if there's only one pod don't prompt

--- glance/logs-glances.sh
+++ glance/logs-glances.sh
@@ -4,12 +4,23 @@ SVC=glance-internal
 #SVC=glance-external
 #SVC=glance-single
 
+i=0
 echo "Which pod do you want logs for?"
 for POD in \
     $(oc get pods -l service=$SVC --no-headers -o custom-columns=":metadata.name"); do
     echo $POD
+    ((i++))
 done
-read -p "Select the pod by number, (e.g. '0' for ${SVC}-api-0) " NUM
+if [ $i -eq 0 ]; then
+    echo "No pods were found matching $SVC"
+    exit 0
+fi
+if [ $i -gt 0 ]; then
+    read -p "Select the pod by number, (e.g. '0' for ${SVC}-api-0) " NUM
+else
+    echo "There's only one POD so taking it"
+    NUM=0
+fi
 POD=${SVC}-api-${NUM}
 echo $POD
 oc logs $POD $@
