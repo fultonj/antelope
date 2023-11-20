a1b2545a0c14237db4029a36e7f0add0d114982c
John Fulton <fulton@redhat.com>
2023-11-20 14:13:16 -0500

    switch from glance-config-data to glance-external-config-data

--- glance/conf-glance.sh
+++ glance/conf-glance.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-for KEY in $(oc get secret glance-config-data -o json | jq -r '.data | keys[]'); do
+# SVC=glance-config-data
+# Internal and External glance store their config here
+SVC=glance-external-config-data
+
+for KEY in $(oc get secret $SVC -o json | jq -r '.data | keys[]'); do
     echo "> $KEY"
     echo "---"
-    oc get secret glance-config-data -o json | jq -r --arg KEY "$KEY" '.data[$KEY]' | base64 -d
+    oc get secret $SVC -o json | jq -r --arg KEY "$KEY" '.data[$KEY]' | base64 -d
     echo "---"
 done
