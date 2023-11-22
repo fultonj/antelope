87646d070956472dbdb799d73963ac73ee3f5562
John Fulton <fulton@redhat.com>
2023-11-21 20:51:44 -0500

    have phased option for the 3 parts of image-create-via-import

--- glance/test-import.sh
+++ glance/test-import.sh
@@ -3,11 +3,13 @@ source functions.sh
 
 CLEAN=1
 WEB=0
-GLANCE_CLI=0
+GLANCE_CLI=1
+PHASED=1
 
 NAME=cirros-$(date +%s)
 CIR=cirros-0.5.2-x86_64-disk.img
 URL=http://download.cirros-cloud.net/0.5.2/$CIR
+STAGE_PATH=/var/lib/glance
 
 rbd -p images ls -l
 
@@ -24,12 +26,23 @@ else
         oc rsh -t --shell='/bin/sh' openstackclient curl $URL -o $CIR
     fi
     if [ $GLANCE_CLI -gt 0 ]; then
-        glance --verbose image-create-via-import \
-               --disk-format raw \
-               --container-format bare \
-               --name $NAME \
-               --file $CIR \
-               --import-method glance-direct
+	if [ $PHASED -gt 0 ]; then
+            glance --verbose image-create \
+		   --disk-format raw \
+		   --container-format bare \
+		   --name $NAME
+	    ID=$(openstack image show $NAME -c id -f value | strings)
+	    glance image-stage --progress --file $CIR $ID
+	    bash cmd-glances.sh ls -l $STAGE_PATH
+	    glance image-import --import-method glance-direct $ID
+	else
+            glance --verbose image-create-via-import \
+		   --disk-format raw \
+		   --container-format bare \
+		   --name $NAME \
+		   --file $CIR \
+		   --import-method glance-direct
+	fi
     else
         openstack image create \
                   --disk-format raw \
@@ -39,12 +52,16 @@ else
     fi
 fi
 
+ID=$(openstack image show $NAME -c id -f value | strings)
+
 until $(openstack image show $NAME -c status -f value | grep -q active); do
     echo -n "."; sleep 1;
 done
 echo ""
 
 openstack image show $NAME
+glance image-show $ID
+glance image-tasks $ID
 rbd -p images ls -l
 
 if [ $CLEAN -gt 0 ]; then
