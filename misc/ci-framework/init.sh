a8c7f1ccc484be338b72d8b6861e02129cc50dfa
John Fulton <fulton@redhat.com>
2024-08-08 14:22:33 -0400

    init git config pull.rebase true

--- misc/ci-framework/init.sh
+++ misc/ci-framework/init.sh
@@ -5,6 +5,7 @@ if [[ $1 == 'git' ]]; then
     git config --global user.email fulton@redhat.com
     git config --global user.name "John Fulton"
     git config --global push.default simple
+    git config --global pull.rebase true
     git clone git@github.com:fultonj/antelope.git
 fi
 
