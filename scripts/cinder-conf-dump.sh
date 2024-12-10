d1fb0eba2675bf576f48c1e821608f1060454a7e
John Fulton <fulton@redhat.com>
2024-12-10 17:59:43 -0500

    use 'oc get pod' to get pod name

--- scripts/cinder-conf-dump.sh
+++ scripts/cinder-conf-dump.sh
@@ -4,7 +4,7 @@ CONF="/etc/cinder/cinder.conf.d"
 CMD="ls $CONF | xargs -I{} cat $CONF/{}"
 
 for POD in $(oc get pods | grep cinder | awk {'print $1'}); do
-    CON=$(echo $POD | sed -E 's/cinder-[^-]+-([a-z]+).*/cinder-\1/')
+    CON=$(oc get pod "$POD" -o jsonpath='{.spec.containers[0].name}')
     if [[ $POD == *0 ]]; then
         echo -e "# POD: $POD\n# ---"
 	oc rsh -c $CON $POD sh -c "$CMD"
